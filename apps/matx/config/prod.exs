ed87c466421beb79d52193fd5e306b8a3ec49d29
Arian Vassili <arian@vassili.se>
2020-07-05 03:22:47 +0200

    Host nil

--- apps/matx/config/prod.exs
+++ apps/matx/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :matx, MatxWeb.Endpoint,
-  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "matx.se", port: 80],
+  url: [host: nil, port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
