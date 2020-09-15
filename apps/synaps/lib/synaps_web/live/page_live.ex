adb0e28518ee26db59eca893645f0d62121ed368
Arian Vassili <arian@vassili.se>
2020-09-16 01:54:49 +0200

    Demo2 test

--- apps/synaps/lib/synaps_web/live/page_live.ex
+++ apps/synaps/lib/synaps_web/live/page_live.ex
@@ -3,6 +3,24 @@ defmodule SynapsWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, personal_number: nil, loading: false, resp: nil, ref: nil, error: false)}
+  end
+
+  def handle_event("sign", %{"personal_number" => pnumb}, socket) do
+    case ExBankID.sign("1.1.1.1", "Tjobre sign pls", personal_number: pnumb, user_non_visible_data: "troll") do
+      {:ok, sign} ->
+        {:noreply, assign(socket, resp: sign, ref: sign.orderRef, error: false)}
+      _ ->
+        {:noreply, assign(socket, error: true)}
+    end
+  end
+
+  def handle_event("sign", %{"personal_number" => pnumb}, socket) do
+    case ExBankID.sign("1.1.1.1", "Tjobre sign pls", personal_number: pnumb, user_non_visible_data: "troll") do
+      {:ok, sign} ->
+        {:noreply, assign(socket, resp: sign, ref: sign.orderRef, error: false)}
+      _ ->
+        {:noreply, assign(socket, error: true)}
+    end
   end
 end
