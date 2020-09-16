c4e8aef076a6c3fd4984bacdbff9fa619a2ce44b
Arian Vassili <arian@vassili.se>
2020-09-16 19:30:42 +0200

    Custom subdomain for plausible, added hotjar

--- apps/synaps/assets/js/app.js
+++ apps/synaps/assets/js/app.js
@@ -49,3 +49,15 @@ let select = s => document.querySelector(s),
 
 // Import specific page views
 import './views/init';
+
+(function(h, o, t, j, a, r) {
+    h.hj = h.hj || function() {
+        (h.hj.q = h.hj.q || []).push(arguments)
+    };
+    h._hjSettings = { hjid: 1995593, hjsv: 6 };
+    a = o.getElementsByTagName('head')[0];
+    r = o.createElement('script');
+    r.async = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
