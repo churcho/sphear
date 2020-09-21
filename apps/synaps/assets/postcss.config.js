591cc9542459e898b84d6c7e159f8175579730e8
Arian Vassili <arian@vassili.se>
2020-09-21 02:48:03 +0200

    remove postcss-fail-on-warn

--- apps/synaps/assets/postcss.config.js
+++ apps/synaps/assets/postcss.config.js
@@ -1,8 +1,7 @@
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('autoprefixer'),
-        require('postcss-fail-on-warn')
+        require('autoprefixer')
     ],
     options: {
         from: "tailwind.css"
