c54b29b2c3d7a4866618fd89c40a7f5d05c79552
Arian Vassili <arian@vassili.se>
2020-09-28 12:40:12 +0200

    Fix

--- apps/db/lib/release.ex
+++ apps/db/lib/release.ex
@@ -4,12 +4,12 @@ defmodule Db.Release do
     """
     @app :db
   
-    def create_and_migrate() do
+    def create_and_migrate do
       createdb()
       migrate()
     end
 
-    def createdb() do
+    def createdb do
       for repo <- repos() do
         case repo.__adapter__.storage_up(repo.config) do
           :ok -> :ok
@@ -19,7 +19,7 @@ defmodule Db.Release do
       end
     end
 
-    def migrate() do
+    def migrate do
       for repo <- repos() do
         {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
       end
@@ -33,24 +33,4 @@ defmodule Db.Release do
       Application.load(@app)
       Application.fetch_env!(@app, :ecto_repos)
     end
-
-    def createdb do
-      # Start postgrex and ecto
-      IO.puts "Starting dependencies..."
-  
-      # Start apps necessary for executing migrations
-      Enum.each(@start_apps, &Application.ensure_all_started/1)
-  
-      Enum.each(@myapps, &create_db_for/1)
-      IO.puts "createdb task done!"
-  
-    end
-  
-    def create_db_for(app) do
-      for repo <- get_repos(app) do
-        :ok = ensure_repo_created(repo)
-      end
-    end
-  
-    
   end
