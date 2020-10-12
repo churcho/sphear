522242c91817075971e8452925629db02bbec1e6
Arian Vassili <arian@vassili.se>
2020-10-12 16:47:00 +0200

    New animation colors

--- apps/synaps/assets/js/app.js
+++ apps/synaps/assets/js/app.js
@@ -45,7 +45,7 @@ Hooks.kontaktSent = {
             i, dot;
 
         // create 80 dot elements and put them in an array
-        for (i = 0; i < 100; i++) {
+        for (i = 0; i < 60; i++) {
             dot = document.createElement("div");
             dot.setAttribute("class", "dot");
             bg.appendChild(dot);
@@ -54,8 +54,9 @@ Hooks.kontaktSent = {
 
         //set the initial position of all the dots, and pick a random color for each from an array of colors
         gsap.set(dots, {
-            backgroundColor: "random([#663399,#84d100,#cc9900,#0066cc,#993333])",
-            scale: "random(0.4, 1)",
+            //backgroundImage: "random([\"linear-gradient(to right, #B83280, #E53E3E)\"])",
+            backgroundColor: "random([#B83280,#E53E3E,#ED8936,#FAF089,#48BB78,#4299E1])",
+            scale: "random(0.3, 0.5)",
             x: document.body.clientWidth / 2,
             y: document.body.clientHeight
         });
@@ -65,10 +66,10 @@ Hooks.kontaktSent = {
             duration: 15,
             physics2D: {
                 velocity: "random(200, 450)",
-                angle: "random(250, 290)",
+                angle: "random(260, 280)",
                 gravity: 85
             },
-            delay: "random(0, 3)"
+            delay: "random(0, 2)"
         });
     }
 }
